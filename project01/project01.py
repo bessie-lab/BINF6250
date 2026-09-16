d4bc47bbf66474bd9511995b7fd66aaa7446fa8b
rtbaskaran-2026 <baskaran.aa@northeastern.edu>
2026-09-16 00:51:29 -0400

    Update project01.py with read_file function
    read_file function is added to the code

--- project01/project01.py
+++ project01/project01.py
@@ -39,4 +39,37 @@ def parse_line(line):
 
 
     return []
+
+def read_file(file):
     
+    """
+    Reads a vcf file, takes each line, runs it through the parse line function above and updates a growing dictionary 
+    with the occurrence of a disease as it shows up in the vcf. 
+    
+    Returns a dictionary with all the times a disease is detected in the vcf file based on parameters from parse_line()
+
+    If the AF_EXAC parameter isn't found or is greater than or equal to 0.0001, the disease isn't counted. 
+    If the AF_EXAC parameter is below 0.0001, the variant is considered rare and all associated diseases are reported.
+    """
+    
+    vcf_disease_count = {}
+
+    with open(file, "r") as f:
+        for line in f:
+            if line.startswith("#"):
+                continue
+            else:
+                parse_outputs = parse_line(line)
+
+                # If a disease was reported from the parse_line function in a list
+                if parse_outputs is not None:
+                    for disease in parse_outputs:
+                        if disease not in vcf_disease_count.keys():
+                            vcf_disease_count[disease] = 1
+                        else:
+                            vcf_disease_count[disease] += 1
+
+    return vcf_disease_count
+
+if __name__ == "__main__":
+    pprint(read_file("clinvar_20190923_short.vcf"))
